f71a5952b606615ef9ca77532f6394d30575e4f5
rahul-narang <narang.rahul1999@gmail.com>
2023-12-27 21:08:26 +0530

    init

---
+++ src/main/java/com/rnanon/ei/EiApplication.java
@@ -0,0 +1,13 @@
+package com.rnanon.ei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rnanon/ei/EiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rnanon.ei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
